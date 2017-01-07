4099fdf6322a373ddd9676dd0a55b74e0a09c624
Cindy <Cindy@admindeMacBook-Pro-2.local>
2017-01-07 18:17:31 +0800

    tijiao

--- CDNotepad/Classes/UILayer/HomePage/CDHomeViewController.m
+++ CDNotepad/Classes/UILayer/HomePage/CDHomeViewController.m
@@ -10,6 +10,8 @@
 
 @interface CDHomeViewController ()
 
+@property (nonatomic,strong) UIButton *buttonTitle;
+
 @end
 
 @implementation CDHomeViewController
@@ -17,21 +19,62 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    
+    [self initButtonItem];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)initTitleView
+{
+    UIButton *button = [[UIButton alloc] init];
+    [button ]
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)initButtonItem
+{
+    UIButton *rightButtonOne = [[UIButton alloc] init];
+    [rightButtonOne setImage:[UIImage imageNamed:@"home_navigation_left_calendar_icon"] forState:UIControlStateNormal];
+    [rightButtonOne setTintColor:[UIColor whiteColor]];
+    rightButtonOne.cd_size = CGSizeMake(28, 28);
+    rightButtonOne.tag = 1;
+    // 监听按钮点击
+    [rightButtonOne addTarget:self action:@selector(rightButtonPressEvent:) forControlEvents:UIControlEventTouchUpInside | UIControlEventTouchUpOutside];
+    
+    UIButton *rightButtonTwo = [[UIButton alloc] init];
+    [rightButtonTwo setImage:[UIImage imageNamed:@"home_navigation_left_calendar_icon"] forState:UIControlStateNormal];
+    [rightButtonTwo setTintColor:[UIColor whiteColor]];
+    rightButtonTwo.cd_size = CGSizeMake(28, 28);
+    rightButtonTwo.tag = 2;
+    // 监听按钮点击
+    [rightButtonTwo addTarget:self action:@selector(rightButtonPressEvent:) forControlEvents:UIControlEventTouchUpInside | UIControlEventTouchUpOutside];
+    
+    self.navigationItem.leftBarButtonItems = @[[[UIBarButtonItem alloc] initWithCustomView:rightButtonTwo],[[UIBarButtonItem alloc] initWithCustomView:rightButtonOne]];
 }
-*/
+
+
+#pragma mark - IBAction Method
+- (void)rightButtonPressEvent:(UIButton *)button
+{
+    
+}
+
+#pragma mark - Getter Method
+- (UIButton *)buttonTitle
+{
+    if (_buttonTitle == nil) {
+        _buttonTitle = [[UIButton alloc] init];
+        
+        UIImageView *imageView = [[UIImageView alloc] init];
+        imageView.contentMode = UIViewContentModeScaleAspectFit;
+        imageView.image = [UIImage imageNamed:@"home_navigation_left_calendar_icon"];
+        [_buttonTitle addSubview:imageView];
+//        [imageView mas_makeConstraints:^(MASConstraintMaker *make) {
+//            make.left.equalTo()
+//        }]
+        
+        
+    }
+}
+
 
 @end
